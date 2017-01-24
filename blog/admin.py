628fcc9f1645360a4e0b99cf6637110d4ca253b1
vitamin <dev.suh@gmail.com>
2017-01-25 03:32:19 +0900

    added view and template for detailed blog post as well as CSS for the site

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 # blog/admin.py
 
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 admin.site.register(Post)
+admin.site.register(Comment)
 
--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.post_list, name='post_list'),		# ^$ 문자열이 아무것도 없는 경우
+	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),		# blog/post_detail.html
 ]
