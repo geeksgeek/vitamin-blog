fda2b331ec6c0f626ebd33c03c0ba530fc9185c7
vitamin <dev.suh@gmail.com>
2017-01-25 11:43:52 +0900

    added views to create/edit blog post inside the site

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.post_list, name='post_list'),		# ^$ 문자열이 아무것도 없는 경우
-	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),		# blog/post_detail.html
+	url(r'^$', views.post_list, name='post_list'),								# post_list, ^$ 문자열이 아무것도 없는 경우
+	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),		# post_detail 
+	url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),		# post_edit
+	url(r'^post/new/$', views.post_new, name='post_new'),						# post_new 
 ]
